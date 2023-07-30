eb74e1ef5a2cd316ce4acfda6c13b3520b532127
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-07-30 17:38:22 +0300

    Add Card component

---
+++ src/app/components/MealCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import Chip from '@mui/material/Chip';
+
+interface MealData {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+  strCategory: string;
+  strTags: string;
+}
+
+interface Meal {
+  meal: MealData
+}
+
+const MealCard: React.FC<Meal> = ({ meal }) => {
+  return (
+    <Card sx={{
+      backgroundColor: "#f0c18d",
+    }}>
+      <CardActionArea>
+        <CardMedia
+          component = "img"
+          height = "300px"
+          image = {meal.strMealThumb}
+          alt = {meal.strMeal}
+        />
+        <CardContent >
+          <Typography gutterBottom variant="h5">{meal.strMeal}</Typography>
+          <Typography variant="body2">{meal.strCategory}</Typography>
+          {
+            meal.strTags ? meal.strTags.split(',').map((tag) => (
+              <Chip label={tag} key={tag} sx={{
+                mr: "10px",
+                mt: "10px",
+                backgroundColor: "#dea55f",
+              }}/>
+            ))
+            : null
+          }
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default MealCard
