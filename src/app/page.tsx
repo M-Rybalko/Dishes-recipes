cd62751d71af36dafee91688e68d9a0c231b26c0
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-07-30 23:11:05 +0300

    Add getStaticProps function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Hero from './components/Hero';
+import CardBlock from './components/CardBlock';
+import axios from 'axios';
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="PageContent">
       <Hero />
+      <CardBlock />
     </main>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  try {
+    const response = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
+    const meals = response.data.meals;
+
+    return meals;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
 }
+
+export default Home;
